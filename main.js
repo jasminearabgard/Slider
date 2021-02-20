4e7feadb03e1573c690eb4289d14353d903151b9
jasminearabgard <yasaman.arabgard@outlook.com>
2021-02-21 00:15:50 +0330

    start js

---
+++ main.js
@@ -0,0 +1,6 @@
+let image = document.getElementsByClassName("image");
+let slides = document.getElementsByClassName("slides");
+let dot = document.getElementsByClassName("dot");
+let nav = document.getElementsByClassName("navigation");
+let next = document.getElementsByClassName("fa-angle-left");
+let prev = document.getElementsByClassName("fa-angle-right");
